c9657dc4bc97cb9a95f37f2088135a4bb0f8aa59
marilia <maritamisguemacedo@gmail.com>
2022-12-28 15:12:10 -0300

    [Feat] Add React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
---
+++ src/Router.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+export const Router = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/historico" element={<History />} />
+    </Routes>
+  )
+}
