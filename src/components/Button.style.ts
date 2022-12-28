74b74c5b68cb5ceae606c49553d90a7439dd03d6
marilia <maritamisguemacedo@gmail.com>
2022-12-28 13:16:31 -0300

    [Feat] Create button component

---
+++ src/components/Button.style.ts
@@ -0,0 +1,24 @@
+import styled, {css} from "styled-components";
+
+export type ButtonVariant = 'primary' |'secondary'|'danger'|'success'; 
+
+interface ButtonContainerProps{
+    variant: ButtonVariant;
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secondary: 'blue',
+    danger: 'red',
+    success: 'green'
+}
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+    height: 40px;
+    width: 100px;
+
+    ${
+        props => {
+            return css`background-color: ${buttonVariants[props.variant]}`
+        }
+    }
+`
